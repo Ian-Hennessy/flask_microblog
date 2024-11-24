9a465ef3584c69e646c3ef63593fab7219e715a3
Ian-Hennessy <ihennessy3@gatech.edu>
2024-11-24 15:36:09 -0500

    Migrated HTML to dedicated templates directory

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,9 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'Username': 'Ian'}
-    return '''
-        <html> 
-            <head>
-                <title>Home Page - microblog<title>
-            </head>
-            <body>
-                <h1> Hello, ''' + user['username'] + '''!<h1>
-            </body>
-        </html>
-        '''
+    return render_template('index.html', title = "HOME", user = user)

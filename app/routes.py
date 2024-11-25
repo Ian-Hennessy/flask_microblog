8ce3e962bea11822de81f12af6b228f60bb01530
Ian-Hennessy <ihennessy3@gatech.edu>
2024-11-25 16:46:28 -0500

    Added a url for the login view, fixed a typo in config.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,5 +16,8 @@ def index():
             'body': 'second post'
         }
     ]
-    print(posts)
     return render_template('index.html', title="HOME", user=user, posts=posts)
+
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign In", form=form)

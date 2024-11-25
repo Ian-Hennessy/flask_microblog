512e5822645666300cf3c72c0d79696489ba30eb
Ian-Hennessy <ihennessy3@gatech.edu>
2024-11-25 12:55:23 -0500

    Added for loop functionality for a loop of posts in blog

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'Username': 'Ian'}
+    posts = [
+        {
+            'author': {'Username': 'Ian'},
+            'body': 'first post'
+        },
+        {
+            'author': {'Username': 'John'},
+            'body': 'second post'
+        }
+    ]
+
     return render_template('index.html', title = "HOME", user = user)

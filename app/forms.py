f04ecefbfbc59a16b9bb4b56edada30b4c06a51a
Ian-Hennessy <ihennessy3@gatech.edu>
2024-11-27 06:13:56 -0500

    Added login verification functionality to index.html, base.html, routes.py

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+# a new class which defines a form for logigng into the web application 
 class LoginForm(FlaskForm):
-    username = StringField('Username:', validators=DataRequired)
-    password = StringField('Password:', validators=DataRequired)
+    username = StringField('Username:', validators=[DataRequired()])
+    password = StringField('Password:', validators=[DataRequired()])
     remember_me = BooleanField('Remember me:')
     submit = SubmitField('Sign In')

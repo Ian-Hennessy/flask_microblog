38aa775ddcdc7eb51369b4a781c786c73b92d681
Ian-Hennessy <ihennessy3@gatech.edu>
2024-11-27 06:50:10 -0500

    Added imports for SQLAlchemy and migrate. Dependencies updated in __init__ and config

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    # Establish Secret Key for CSRF attack robustness
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Theresasnakeinmyboot17!'
+
+    # Establish a database reference for storing, accessing, 
+    # etc data on the application
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
